24d1036a81fe0703fa0ca1436fa5b3aacfb66f3b
sinclairzx81 <haydn.developer@gmail.com>
2022-06-30 01:49:12 +0900

    add t.fail() for non-thrown exception

--- tests/index.js
+++ tests/index.js
@@ -33,6 +33,7 @@ tap.test('should not compile schema with unknown keywords', async t => {
     }, (_req, _res) => { })
     try {
         await fastify.listen({ port: 5000 }) // expect throw
+        t.fail()
     } catch {
         await fastify.close()
         t.pass()
